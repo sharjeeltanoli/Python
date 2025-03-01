53cf21d49809b0151789309134a27cb71bc77543
Muhammad Sharjeel <160207515+sharjeeltanoli@users.noreply.github.com>
2025-03-01 11:19:19 +0500

    Add files via upload

---
+++ step02_Variables&DataTypes/data.py
@@ -0,0 +1,38 @@
+# Variables with Data Types
+
+#1. Integer (int)
+x : int = 5
+print(x)  
+
+#2. Float (float)
+y : float = 5.0
+print(y)
+
+#3. String (str)
+name : str = "Sharjeel"
+print(name)
+
+#4. Boolean (bool)
+isPresent : bool = True  # True or False
+print(isPresent)
+
+#5. List (list)  Mutable (can be modified add, remove, change elements)
+Count : list = [1,2,3,4,5]
+Count.append(6)
+print(Count)
+
+#6. Tuple (tuple)   # Immutable
+Count2 : tuple = (1,2,3,4,5)
+print(Count2)
+
+#7. Dictionary (dict)
+Person : dict = {"name": "Sharjeel", "age": 28}
+print(Person)
+
+#8. Set (set)
+Numbers : set = {1,2,3,4,5}
+print(Numbers)
+
+#9. None (None)
+z = None
+print(z)
