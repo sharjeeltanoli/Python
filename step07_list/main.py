75ecb3362a9fdfcb73c15e815ee7e53512de2f55
Muhammad Sharjeel <160207515+sharjeeltanoli@users.noreply.github.com>
2025-04-04 21:18:22 +0500

    Add files via upload

---
+++ step07_list/main.py
@@ -0,0 +1,43 @@
+#List
+#          0          1         2        3       4
+items = ["apple", "banana", "cherry", "mango", "grapes"]
+
+#getting all the items in the list
+print(f"all Items: {items}" )
+
+#get the length of the list
+print("Length of item:", len( items ))
+
+#getting the first item in the list
+print( "first item:", items[0] ) # apple
+
+#getting the last item in the list
+print( "last item:", items[-1] ) # grapes
+
+#list Slicing
+
+print("Slicing items:", items[1:3] ) # ['banana', 'cherry']
+
+#Checking if an item is in the list
+print( "apple" in items ) # True
+print( "orange" in items ) # False
+
+#Adding items to the list
+items.append("orange") # adding at the end of the list
+print("After adding orange:", items )
+
+#Adding items at a specific index
+items.insert(2, "kiwi") # adding at index 2
+print("After adding at index 2:", items )
+
+#Removing items from the list
+items.remove("banana") # removing by value
+print("After removing banana:", items )
+
+items.pop() # removing the last item
+print("After removing last item:", items )
+
+items.pop(1) # removing item at index 1
+print("After removing item at index 1:", items )
+
+
